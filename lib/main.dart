7b0c335674c58aac3a0e7730aa2fa3e3331e1a96
novikov <novikov.appstudio@gmail.com>
2022-03-30 22:57:07 +0300

    Настроен слой данных

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:places/app.dart';
-import 'package:places/data/rest/rest_client.dart';
 
 void main() {
   LicenseRegistry.addLicense(() async* {
@@ -11,14 +10,4 @@ void main() {
   });
 
   runApp(const App());
-
-  final restClient = RestClient.getInstance(
-    baseUrl: 'http://jsonplaceholder.typicode.com',
-  );
-
-  restClient.get<String>('/users').then(
-        (result) => debugPrint('RESULT: $result'),
-        // ignore: avoid_types_on_closure_parameters
-        onError: (Object error) => debugPrint('$error'),
-      );
 }
