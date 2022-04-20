219b1c636eb011a3a8a3ca96444c3f3910671e02
novikov <novikov.appstudio@gmail.com>
2022-04-21 00:37:15 +0300

    Задержка поиска переведена с таймера на поток

--- lib/ui/screen/sight_search_screen.dart
+++ lib/ui/screen/sight_search_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:places/domain/sight.dart';
@@ -18,6 +16,7 @@ import 'package:places/ui/widget/controls/simple_app_bar.dart';
 import 'package:places/ui/widget/empty_list.dart';
 import 'package:places/ui/widget/search_history.dart';
 import 'package:redux/redux.dart';
+import 'package:rxdart/rxdart.dart';
 
 /// Экран "Поиск мест" по названию.
 ///
@@ -34,16 +33,28 @@ class SightSearchScreen extends StatefulWidget {
 
 class _SightSearchScreenState extends State<SightSearchScreen> {
   late final Store<AppState> _store;
-  late final _DelayedSearch _delayedSearch;
   final _controller = TextEditingController();
-  String? _lastSearch = '';
+  final _debounce = PublishSubject<String>();
 
   @override
   void initState() {
     super.initState();
     _store = StoreProvider.of<AppState>(context, listen: false);
-    _delayedSearch = _DelayedSearch(milliseconds: 500, callback: _search);
-    _controller.addListener(_onTextChange);
+    _debounce.stream
+        .debounce(
+          (value) => TimerStream<String>(
+            value,
+            Duration(
+              milliseconds:
+                  value.trim().isEmpty || value.endsWith(' ') ? 0 : 500,
+            ),
+          ),
+        )
+        .map((value) => value.trim())
+        .distinct()
+        .where((value) => value.isEmpty || value.length > 2)
+        .listen(_search);
+    _controller.addListener(_onTextChanged);
   }
 
   @override
@@ -92,9 +103,7 @@ class _SightSearchScreenState extends State<SightSearchScreen> {
               /// Поиск завершен.
               found: (sights) => ListView.separated(
                 itemCount: sights.length,
-                itemBuilder: (_, index) => _SightListTile(
-                  sight: sights[index],
-                ),
+                itemBuilder: (_, index) => _SightListTile(sight: sights[index]),
                 separatorBuilder: (_, index) => const Divider(
                   height: 0.8,
                   indent: 88,
@@ -126,44 +135,23 @@ class _SightSearchScreenState extends State<SightSearchScreen> {
 
   @override
   void dispose() {
-    _controller.dispose();
-    _delayedSearch.dispose();
+    _controller
+      ..removeListener(_onTextChanged)
+      ..dispose();
+    _debounce.close();
     super.dispose();
   }
 
-  /// Callback на изменение содержимого поля ввода.
-  void _onTextChange() {
-    final text = _controller.text;
-
-    //Если ввели только 1-2 символа или текст не изменился, ничего не делаем
-    if (text.isNotEmpty && text.length < 3 || text == _lastSearch) {
-      return;
-    }
-    _lastSearch = text;
-
-    // Если поле пустое, показыаем историю
-    if (text.trim().isEmpty) {
-      _delayedSearch.cancel();
-      _store.dispatch(const SearchState.initial());
-    } // Если ввели целое слово, сразу начинаем поиск
-    else if (text.endsWith(' ')) {
-      _delayedSearch.cancel();
-      _search(text.trimRight());
-    } // Отложенный поиск
-    else {
-      _delayedSearch(text);
-    }
+  /// Поиск мест по подстроке.
+  void _search(String text) {
+    _store.dispatch(
+      text.isNotEmpty ? SearchAction.start(text) : const SearchState.initial(),
+    );
   }
 
-  /// Поиск мест по подстроке.
-  Future<void> _search(String text) async {
-    final pattern = _controller.text;
-
-    // Так как поиск начинаем с задержкой, поле ввода могли очистить,
-    // в этом случае отображаем историю
-    _store.dispatch(pattern.isEmpty
-        ? const SearchState.initial()
-        : SearchAction.start(pattern));
+  /// Callback на изменения текста в поле ввода.
+  void _onTextChanged() {
+    _debounce.add(_controller.text);
   }
 }
 
@@ -182,7 +170,7 @@ class _SightListTile extends StatelessWidget {
 
     return ListTile(
       leading: DarkenImage(
-        url: sight.urls.first,
+        url: sight.urls.isNotEmpty ? sight.urls.first : '',
         size: const Size.square(56.0),
         borderRadius: 12.0,
       ),
@@ -195,32 +183,5 @@ class _SightListTile extends StatelessWidget {
   /// Показ экрана детализации.
   void _showDetails(BuildContext context) {
     context.pushBottomSheet(AppRoutes.details, args: sight.id);
-  }
-}
-
-/// Класс для задержки реакции на изменение.
-class _DelayedSearch {
-  final void Function(String) callback;
-  final Duration _delay;
-  Timer? _timer;
-
-  _DelayedSearch({
-    required int milliseconds,
-    required this.callback,
-  }) : _delay = Duration(milliseconds: milliseconds);
-
-  void call(String text) {
-    _timer?.cancel();
-    _timer = Timer(_delay, () {
-      callback(text);
-    });
-  }
-
-  void cancel() {
-    _timer?.cancel();
-  }
-
-  void dispose() {
-    cancel();
   }
 }
