6c8440443a9eed5f9e7b1c0714e4fada78cec52d
novikov <novikov.appstudio@gmail.com>
2022-05-06 13:10:17 +0300

    Ограничение на кол-во отображаемых элементов истории вынесено в Ui-слой.

--- lib/data/repository/db_search_history_repository.dart
+++ lib/data/repository/db_search_history_repository.dart
@@ -2,18 +2,14 @@ import 'package:places/data/database/database.dart';
 import 'package:places/domain/repository/search_history_repository.dart';
 
 /// Хранилище истории поиска в базе данных.
-///
-/// Хранит всю историю, но отображает [_maxSize] последних элементов.
 class DbSearchHistoryRepository implements SearchHistoryRepository {
-  static const int _maxSize = 20;
-
   final Database _database;
 
   const DbSearchHistoryRepository(this._database);
 
   @override
-  Future<List<String>> items() async =>
-      _database.historyGetAll(limit: _maxSize);
+  Future<List<String>> items({int? lastCount}) async =>
+      _database.historyGetAll(limit: lastCount);
 
   @override
   Future<void> add(String value) async => _database.historyAdd(value);
--- lib/domain/repository/search_history_repository.dart
+++ lib/domain/repository/search_history_repository.dart
@@ -2,7 +2,9 @@
 abstract class SearchHistoryRepository {
   /// Элементы должны быть отсортированы в порядке, обратном порядку добавления
   /// (последние добавленные - в начале списка)
-  Future<List<String>> items();
+  ///
+  /// Позволяет запросить все элементы, либо [lastCount] последних элементов.
+  Future<List<String>> items({int? lastCount});
 
   Future<void> add(String value);
 
